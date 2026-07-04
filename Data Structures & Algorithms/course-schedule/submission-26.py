af221ff89ba0a82f0e30e28d355a252b7ea09ee5
Peiyu-Li <69741809+Peiyu-Li-Tara@users.noreply.github.com>
2026-07-04 00:27:14 -0700

    Add: course-schedule - submission-26

---
+++ Data Structures & Algorithms/course-schedule/submission-26.py
@@ -0,0 +1,46 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if not prerequisites:
+            return True
+        if len(prerequisites) == 1 and numCourses <= 1:
+            return True
+
+        # build the adjlist
+        graph = {i: [] for i in range(numCourses)}
+        for course, prerequisite in prerequisites:
+            graph[course].append(prerequisite)
+        print(graph)
+        
+        def bfs(course):
+            visit, q = set(), deque()
+            visit.add(course)
+            q.append(course)
+            while q:
+                for _ in range(len(q)):
+                    c = q.popleft()
+                    for pre in graph[c]:
+                        if pre in visit:
+                            print(course, c, pre)
+                            return False
+                        q.append(pre)
+                    visit.add(c)
+            return True
+        
+        visit = set()
+        def dfs(course):
+            if not graph[course]:
+                return True
+            if course in visit:
+                return False
+            visit.add(course)
+            for pre in graph[course]:
+                if not dfs(pre):
+                    return False
+            visit.remove(course)
+            graph[course] = []
+            return True
+        
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        return True

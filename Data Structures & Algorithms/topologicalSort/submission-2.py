5d65ddb959ea0e4735742b5984521491b093297d
Peiyu-Li <69741809+Peiyu-Li-Tara@users.noreply.github.com>
2026-07-08 00:08:04 -0700

    Add: topologicalSort - submission-2

---
+++ Data Structures & Algorithms/topologicalSort/submission-2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def topologicalSort(self, n: int, edges: List[List[int]]) -> List[int]:
+        if not edges:
+            return [i for i in range(n)]
+
+        # create adj list
+        adj = {n: [] for n in range(n)}
+        for prev, nxt in edges:
+            adj[prev].append(nxt)
+        
+        visit = set()
+        path = set()
+        ret = []
+        def dfs(node):
+            if not adj[node]:
+                return True
+            if node in visit:
+                return True
+            if node in path:
+                return False
+            path.add(node)
+            for nxt in adj[node]:
+                if not dfs(nxt):
+                    return False
+            path.remove(node)
+            visit.add(node)
+            ret.append(node)
+            return True
+        
+        for i in range(n):
+            if not dfs(i):
+                return []
+        ret.reverse()
+        return ret
+            
+            
